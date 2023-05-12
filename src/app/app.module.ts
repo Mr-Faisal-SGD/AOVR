8b083690faeba7c12c1b22f9fca2673e5235fb14
Mr-Faisal-SGD <faisal450fai@gmail.com>
2023-05-13 01:52:27 +0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgIconsModule } from '@ng-icons/core';
+import { featherArrowDownRight } from '@ng-icons/feather-icons';
+import { lucideArrowDownRight, lucideArrowUpRight } from '@ng-icons/lucide';
 
 import { AppComponent } from './app.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { WrapperComponent } from './partials/wrapper/wrapper.component';
+import { StakeComponent } from './components/stake/stake.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, HeroComponent, WrapperComponent, StakeComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgIconsModule.withIcons({
+      featherArrowDownRight,
+      lucideArrowDownRight,
+      lucideArrowUpRight,
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/stake/stake.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-stake',
+  templateUrl: './stake.component.html',
+  styleUrls: ['./stake.component.css']
+})
+export class StakeComponent {
+
+}
